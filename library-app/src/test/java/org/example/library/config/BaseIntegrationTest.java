e2a97ef3cd84c18e1d2acab0ee02cb2b5d5f1a24
Dmytro Melnyk <dimamel28@gmail.com>
2026-05-09 18:46:28 +0300

    Fix

--- library-app/src/test/java/org/example/library/config/BaseIntegrationTest.java
+++ library-app/src/test/java/org/example/library/config/BaseIntegrationTest.java
@@ -1,11 +1,13 @@
 package org.example.library.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public abstract class BaseIntegrationTest {
 
     private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
